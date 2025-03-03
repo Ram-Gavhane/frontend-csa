c8dbd3a5743c907a090998d09d8715e7f77726d0
Ram Gavhane <ramgavhane28@gmail.com>
2025-03-03 12:09:51 +0530

    test01

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export function Home(){
     return (
-        <div className="grid grid-cols-12 gap-6 mx-17.5 mt-[65px] mb-30">
+        <div className="grid grid-cols-12 gap-6 mx-17.5 mt-[65px] mb-16">
             <div className="col-span-7 h-[230px] bg-[#0F1C27] rounded-[12px] text-[42px] pl-7.5 pt-7.5 shadow-[0_4px_4px_-24px_rgba(52,73,102,0.3)] leading-13.75">Top Educator's of India on <br/>one platform <p className="text-[16px]">Learn from the best at the best price</p></div>
             <div className="col-span-5 h-[230px] rounded-[12px] bg-[rgb(52,73,102)]">Image</div>
             <div className="col-span-4 h-[391px] rounded-[12px] bg-[rgb(52,73,102)]"><img className="w-md" src="src/assets/Frame77.svg" alt="" /><p className="text-[24px] pl-4.5 pt-3">Daily Live Classes</p><p className="text-[14px] pl-4.5 pt-3 pr-9">Chat with educators, ask questions, answer live polls, and get your doubts cleared - all while the class is going on</p></div>
