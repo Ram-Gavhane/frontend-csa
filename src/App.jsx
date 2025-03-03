7a0fa4b87f0b11422406d879222c1dbd4174e773
Ram Gavhane <ramgavhane28@gmail.com>
2025-03-03 11:48:49 +0530

    Home page - 1st iteration

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import { Layout } from './pages/Layout'
 import { Signup } from './pages/Signup'
 import { Login } from './pages/Login'
+import { Home } from './pages/Home'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path='/signup' element={<Signup />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/home' element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export function Home(){
+    return (
+        <div className="grid grid-cols-12 gap-6 mx-17.5 mt-[65px] mb-30">
+            <div className="col-span-7 h-[230px] bg-[#0F1C27] rounded-[12px] text-[42px] pl-7.5 pt-7.5 shadow-[0_4px_4px_-24px_rgba(52,73,102,0.3)] leading-13.75">Top Educator's of India on <br/>one platform <p className="text-[16px]">Learn from the best at the best price</p></div>
+            <div className="col-span-5 h-[230px] rounded-[12px] bg-[rgb(52,73,102)]">Image</div>
+            <div className="col-span-4 h-[391px] rounded-[12px] bg-[rgb(52,73,102)]"><img className="w-md" src="src/assets/Frame77.svg" alt="" /><p className="text-[24px] pl-4.5 pt-3">Daily Live Classes</p><p className="text-[14px] pl-4.5 pt-3 pr-9">Chat with educators, ask questions, answer live polls, and get your doubts cleared - all while the class is going on</p></div>
+            <div className="col-span-4 h-[391px] rounded-[12px] bg-[rgb(52,73,102)]"><img className="w-md" src="src/assets/Frame78.svg" alt="" /><p className="text-[24px] pl-4.5 pt-3">Practice and Revise</p><p className="text-[14px] pl-4.5 pt-3 pr-9">Learning isn't just limited to classes with our practice section, mock tests and lecture notes shared as PDFs for your revision</p></div>
+            <div className="col-span-4 h-[391px] rounded-[12px] bg-[rgb(52,73,102)]"><img className="w-md" src="src/assets/Frame79.svg" alt="" /><p className="text-[24px] pl-4.5 pt-3">Learn anytime, anywhere</p><p className="text-[14px] pl-4.5 pt-3 pr-9">Chat with educators, ask questions, answer live polls, and get your doubts cleared - all while the class is going on</p></div>
+        </div>
+    )
+}
